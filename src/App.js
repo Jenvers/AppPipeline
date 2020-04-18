31c8287be0c2d7733b53da4f78f263ca9172d69d
unknown <jenncyviro@unisabana.edu.co>
2020-04-17 23:01:51 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+ 
+
+<div class="container" >
+		<div class="header">
+			<img src="imagenes/uni.jpg"  height="85" width="235" alt=""/>
+		</div>
+		<img src="imagenes/infra.png" alt=""/>
+		<div class="row">
+			<div class="col">
+				<h2>Jenncy Villamizar</h2>
+				<img src="imagenes/jenncy.png" height="300" width="300" alt=""/>
+				<p>Estudiante de Noveno Semestre de ingeniería Informática de la Universidad de La Sabana.</p>
+			</div>
+			<div class="col">
+				<h2>Angie Rodriguez</h2>
+				<img src="imagenes/angie.png" height="300" width="300" alt=""/>
+				<p>Estudiante de Noveno Semestre de ingeniería Informática de la Universidad de La Sabana.</p>
+			</div>
+			<div class="col">
+				<h2>Juan F. Vargas</h2>
+				<img src="imagenes/juan.png" height="300" width="300" alt=""/>
+				<p>Estudiante de Noveno Semestre de ingeniería Informática de la Universidad de La Sabana.</p>
+			</div>
+
+
+	</div>
+ </div>
+    
+
+
+                
+  );
+}
+
+export default App;
